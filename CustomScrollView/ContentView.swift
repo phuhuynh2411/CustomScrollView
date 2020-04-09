4b5a87cc27230d080e49a82eac8c2cff7bfee993
Phu Huynh <phu@filesoft.tech>
2020-04-09 23:01:16 +0700

    Initial Commit

---
+++ CustomScrollView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomScrollView
+//
+//  Created by Huynh Tan Phu on 4/9/20.
+//  Copyright © 2020 Filesoft. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
